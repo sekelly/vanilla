2b91fdffe735799bdb0bba922bfcb82a71844966
SE Kelly <94656500+sekelly@users.noreply.github.com>
2025-02-06 19:11:59 -1000

    Create client-view.js
    new

---
+++ client-view.js
@@ -0,0 +1,72 @@
+ZoomMtg.setZoomJSLib('https://source.zoom.us/3.11.0/lib', '/av')
+
+ZoomMtg.preLoadWasm()
+ZoomMtg.prepareWebSDK()
+// loads language files, also passes any error messages to the ui
+ZoomMtg.i18n.load('en-US')
+ZoomMtg.i18n.reload('en-US')
+
+// setup your signature endpoint here: https://github.com/zoom/meetingsdk-sample-signature-node.js
+var signatureEndpoint = 'https://yckfd07uph.execute-api.us-east-1.amazonaws.com/latest'
+var sdkKey = 'PfPapLCavJc2ZBjMkssNeqboLdBOpoAEXQTc'
+var meetingNumber = '95160050210'
+var role = 1
+var disablePreview = 'true'
+var leaveUrl = 'https://sekelly.github.io/webskjscv/'
+var userName = 'ClientView'
+var userEmail = 'client@kelkel.com'
+var passWord = '123'
+// pass in the registrant's token if your meeting or webinar requires registration. More info here:
+// Meetings: https://marketplace.zoom.us/docs/sdk/native-sdks/web/client-view/meetings#join-meeting-with-registration-required
+// Webinars: https://marketplace.zoom.us/docs/sdk/native-sdks/web/client-view/webinars#join-webinar-with-registration-required
+var registrantToken = ''
+
+function getSignature() {
+  fetch(signatureEndpoint, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      meetingNumber: meetingNumber,
+      role: role
+    })
+  }).then((response) => {
+    return response.json()
+  }).then((data) => {
+    console.log(data)
+    startMeeting(data.signature)
+  }).catch((error) => {
+  	console.log(error)
+  })
+}
+
+function startMeeting(signature) {
+
+  document.getElementById('zmmtg-root').style.display = 'block'
+
+  ZoomMtg.init({
+    leaveUrl: leaveUrl,
+    success: (success) => {
+      console.log(success)
+      ZoomMtg.join({
+        signature: signature,
+        sdkKey: sdkKey,
+        meetingNumber: meetingNumber,
+        userName: userName,
+        userEmail: userEmail,
+        passWord: passWord,
+        tk: registrantToken,
+        success: (success) => {
+          console.log(success)
+        },
+        error: (error) => {
+          console.log(error)
+        },
+      })
+    },
+    error: (error) => {
+      console.log(error)
+    }
+  })
+}
